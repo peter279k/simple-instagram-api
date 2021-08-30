87fcb1a2ad473c7836405c08f398927ae1a7d1df
Peter <peter279k@gmail.com>
2021-08-30 18:41:48 +0800

    Replace original package with instagram-user-feed

--- process.php
+++ process.php
@@ -1,29 +1,42 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+declare(strict_types=1);
 
-use Ayesh\InstagramDownload\InstagramDownload;
+use Instagram\Api;
+use Instagram\Exception\InstagramException;
 
-$response = [];
+use Instagram\Model\Media;
+use Psr\Cache\CacheException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+require realpath(dirname(__FILE__)) . '/vendor/autoload.php';
+$credentials = include_once realpath(dirname(__FILE__)) . '/credentials.php';
+
+$cachePool = new FilesystemAdapter('Instagram', 0, __DIR__ . '/../cache');
+
 $url = (string)filter_input(INPUT_POST, 'url');
+$response = [
+    'result' => '',
+];
 
-if ($url === '') {
-    $response['result'] = 'The url param is missed.';
+if ($url == '') {
+    $response['result'] = 'The url param is missed';
     echo json_encode($response);
     exit(0);
 }
 
 try {
-    $client = new InstagramDownload($url);
-    $downloadedUrl = $client->getDownloadUrl();
-    $type = $client->getType();
-    $response['result'] = $downloadedUrl;
-} catch (\InvalidArgumentException $exception) {
-    $error = $exception->getMessage();
-    $response['error'] = $error;
-} catch (\RuntimeException $exception) {
-    $error = $exception->getMessage();
-    $response['error'] = $error;
-}
+    $api = new Api($cachePool);
+    $api->login($credentials->getLogin(), $credentials->getPassword());
 
-echo json_encode($response);
+    $media = new Media();
+    $media->setLink($url);
+
+    $mediaDetailed = $api->getMediaDetailed($media);
+    $response['result'] = $mediaDetailed->getDisplayResources();
+    echo json_encode($response);
+} catch (InstagramException $e) {
+    print_r($e->getMessage());
+} catch (CacheException $e) {
+    print_r($e->getMessage());
+}
